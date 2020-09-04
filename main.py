70587a3ca1bfc01e75fd8d4ac7e3f4f50476622d
d3scmps <jean.descmps@gmail.com>
2020-09-04 16:24:48 +0200

    fixing syntaxic defaults

--- main.py
+++ main.py
@@ -30,16 +30,11 @@ def removing_screen_names_repetition(urls):
 with open("polarisation.csv", "r") as f, open("media_username.csv", "w") as f2:
     file_content = csv.DictReader(f)
     # list of urls list for each entity
-    urls_list = []
     writer = csv.DictWriter(f2, fieldnames=['ID', 'NAME', 'HOME PAGE', 'SCREEN_NAME'])
     writer.writeheader()
     for row in tqdm(file_content):
         if row["type (TAGS)"] != "media":
             continue
-        media = {'ID': row['ID'], 'NAME': row['NAME'], 'HOME PAGE': row['HOME PAGE'], 'URLS': row['PREFIXES AS URL'].split(" ")}
-        urls_list.append(media)
-    for index, entity in enumerate(urls_list):
-        username = removing_screen_names_repetition(entity["URLS"])
+        username = removing_screen_names_repetition(row['PREFIXES AS URL'].split(" "))
         for distinct_username in username:
-            writer.writerow({'ID': entity["ID"], 'NAME': entity["NAME"], 'HOME PAGE': entity["HOME PAGE"], 'SCREEN_NAME': distinct_username})
-      
+            writer.writerow({'ID': row["ID"], 'NAME': row["NAME"], 'HOME PAGE': row["HOME PAGE"], 'SCREEN_NAME': distinct_username})
